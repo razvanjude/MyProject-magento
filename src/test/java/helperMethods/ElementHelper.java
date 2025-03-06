a9afa24eafdf9fc222bec970ddf227d86acd51ce
razvanjude <jude.razvan@gmail.com>
2025-03-06 17:26:29 +0200

    Add My account test

--- src/test/java/helperMethods/ElementHelper.java
+++ src/test/java/helperMethods/ElementHelper.java
@@ -1,9 +1,6 @@
 package helperMethods;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -41,6 +38,19 @@ public class ElementHelper {
 
     public void fillLocator(By locator, String value) {
         waitForVisible(locator);
+        driver.findElement(locator).sendKeys(value);
+    }
+
+    public void fillPressLocator(By locator, String value, Keys key) {
+        waitForVisible(locator);
+        driver.findElement(locator).sendKeys(value);
+        driver.findElement(locator).sendKeys(key);
+    }
+
+
+    public void clearAndFillLocator(By locator, String value) {
+        waitForVisible(locator);
+        driver.findElement(locator).clear();
         driver.findElement(locator).sendKeys(value);
     }
 
--- src/test/java/tests/MyAccount.java
+++ src/test/java/tests/MyAccount.java
@@ -2,16 +2,50 @@ package tests;
 
 import helperMethods.ElementHelper;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
 import sharedData.SharedData;
 
 public class MyAccount extends SharedData {
 
+//    @Test
+//    public void testMethod() {
+//
+//        ElementHelper elementHelper = new ElementHelper(getDriver());
+//
+//        By userOption = By.xpath("(//button[@type='button'])[1]");
+//        elementHelper.clickLocator(userOption);
+//
+//        By myAccountButton = By.xpath("//li[@class='customer-welcome active']//li[1]");
+//        elementHelper.clickLocator(myAccountButton);
+//
+//        By myAccountElement = By.xpath("//span[@class='base']");
+//        String expectedMessage = "My Account";
+//        elementHelper.validateElementText(myAccountElement,expectedMessage);
+//    }
+
     @Test
-    public void testMethod() {
+    public void AddInfoToAddressBook() {
 
         ElementHelper elementHelper = new ElementHelper(getDriver());
 
+        //Face Login
+        By signInElement = By.xpath("//a[contains(text(),'Sign In')]");
+        elementHelper.clickLocator(signInElement);
+
+        By emailElement = By.xpath("//input[@id='email']");
+        String emailValue = "gigi.Constatin4@example.com";
+        elementHelper.fillLocator(emailElement,emailValue);
+
+        By passwordElement = By.xpath("(//input[@id='pass'])[1]");
+        String passwordValue = "Pa$$w0rd.";
+        elementHelper.fillLocator(passwordElement,passwordValue);
+
+        By signInButton = By.xpath("//button[@class='action login primary']");
+        elementHelper.clickJsLocator(signInButton);
+
+
+        //Deschide My Account
         By userOption = By.xpath("(//button[@type='button'])[1]");
         elementHelper.clickLocator(userOption);
 
@@ -21,5 +55,81 @@ public class MyAccount extends SharedData {
         By myAccountElement = By.xpath("//span[@class='base']");
         String expectedMessage = "My Account";
         elementHelper.validateElementText(myAccountElement,expectedMessage);
+
+
+        //Deschide AddressBook
+        By addresBookButton = By.xpath("(//a[normalize-space()='Address Book'])[1]");
+        elementHelper.clickLocator(addresBookButton);
+
+        By addNewAddressButton = By.xpath("//span[normalize-space()='Add New Address']");
+        elementHelper.clickLocator(addNewAddressButton);
+
+        By firstNameElement = By.xpath("//input[@id='firstname']");
+        String firstNameValue = "Razvan";
+        elementHelper.clearAndFillLocator(firstNameElement,firstNameValue);
+
+        By lastNameElement = By.xpath("//input[@id='lastname']");
+        String lastNameValue = "Jude";
+        elementHelper.clearAndFillLocator(lastNameElement,lastNameValue);
+
+        By phoneNumberElement = By.xpath("//input[@id='telephone']");
+        String phoneNumberValue = "0740123321";
+        elementHelper.clearAndFillLocator(phoneNumberElement,phoneNumberValue);
+
+        By streetAddressElement = By.xpath("//input[@id='street_1']");
+        String streetAddressValue = "str. Ion Mester";
+        elementHelper.clearAndFillLocator(streetAddressElement,streetAddressValue);
+
+        By cityElement = By.xpath("//input[@name='city']");
+        String cityValue = "CLuj";
+        elementHelper.clearAndFillLocator(cityElement,cityValue);
+
+        By stateElement = By.xpath("//select[@id='region_id']");
+        String stateValue = "Guam";
+        elementHelper.fillPressLocator(stateElement,stateValue, Keys.ENTER);
+
+        By postalCodeElement = By.xpath("//input[@id='zip']");
+        String postalCodeValue = "200400";
+        elementHelper.clearAndFillLocator(postalCodeElement,postalCodeValue);
+
+        By saveAddressButton = By.xpath("//button[@title='Save Address']");
+        elementHelper.clickLocator(saveAddressButton);
+
+        By saveAddressConfirmationElement = By.xpath("//div[contains(text(),'You saved the address.')]");
+        String expectMessage = "You saved the address.";
+        elementHelper.validateElementText(saveAddressConfirmationElement,expectMessage);
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

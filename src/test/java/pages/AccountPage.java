fbd555afdf79fb8cff009cbd16f09de598717853
razvanjude <jude.razvan@gmail.com>
2025-05-30 19:28:53 +0300

    add some more logs

--- src/test/java/pages/AccountPage.java
+++ src/test/java/pages/AccountPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import helperMethods.ElementHelper;
+import loggerUtility.LoggerUtility;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import pageLocators.AccountLocators;
@@ -17,10 +18,12 @@ public class AccountPage {
 
     public void goToMyAccount() {
         elementHelper.clickLocator(AccountLocators.myAccountButton);
+        LoggerUtility.infoLog("Access My Account");
     }
 
     public void goToUserOption() {
         elementHelper.clickLocator(AccountLocators.userOption);
+        LoggerUtility.infoLog("Go to User option");
     }
 
     public void landOnMyAccountPage(String expectedMessage) {
@@ -29,50 +32,60 @@ public class AccountPage {
 
     public void goToAddressBook() {
         elementHelper.clickLocator(AccountLocators.addresBookButton);
+        LoggerUtility.infoLog("Go to AddressBook");
     }
 
     public void addNewAddress() {
         elementHelper.clickLocator(AccountLocators.addNewAddressButton);
+        LoggerUtility.infoLog("Click new address button");
     }
 
     public void insertFirstName(String firstNameValue) {
         elementHelper.clearAndFillLocator(AccountLocators.firstNameElement, firstNameValue);
+        LoggerUtility.infoLog("Insert firstname");
     }
 
     public void insertLastName(String lastNameValue) {
-               elementHelper.clearAndFillLocator(AccountLocators.lastNameElement, lastNameValue);
+        elementHelper.clearAndFillLocator(AccountLocators.lastNameElement, lastNameValue);
+        LoggerUtility.infoLog("Insert lastname");
     }
 
     public void insertPhoneNumber(String phoneNumberValue) {
-                elementHelper.clearAndFillLocator(AccountLocators.phoneNumberElement, phoneNumberValue);
+        elementHelper.clearAndFillLocator(AccountLocators.phoneNumberElement, phoneNumberValue);
+        LoggerUtility.infoLog("Insert phone number");
     }
 
     public void insertStreetAddress(String streetAddressValue) {
-                elementHelper.clearAndFillLocator(AccountLocators.streetAddressElement, streetAddressValue);
+        elementHelper.clearAndFillLocator(AccountLocators.streetAddressElement, streetAddressValue);
+        LoggerUtility.infoLog("Insert street address");
     }
 
     public void insertCity(String cityValue) {
-                elementHelper.clearAndFillLocator(AccountLocators.cityElement, cityValue);
+        elementHelper.clearAndFillLocator(AccountLocators.cityElement, cityValue);
+        LoggerUtility.infoLog("Insert city name");
     }
 
     public void insertState(String stateValue) {
-               elementHelper.fillPressLocator(AccountLocators.stateElement,stateValue, Keys.ENTER);
+        elementHelper.fillPressLocator(AccountLocators.stateElement,stateValue, Keys.ENTER);
+        LoggerUtility.infoLog("Insert state name");
     }
 
     public void chooseCountry(String countryValue) {
         elementHelper.fillPressLocator(AccountLocators.country, countryValue, Keys.ENTER);
+        LoggerUtility.infoLog("Insert country name");
     }
 
     public void insertZipCode(String postalCodeValue) {
-               elementHelper.clearAndFillLocator(AccountLocators.postalCodeElement, postalCodeValue);
+        elementHelper.clearAndFillLocator(AccountLocators.postalCodeElement, postalCodeValue);
+        LoggerUtility.infoLog("Insert postal code");
     }
 
     public void saveAddress() {
         elementHelper.clickLocator(AccountLocators.saveAddressButton);
+        LoggerUtility.infoLog("Press save button");
     }
 
     public void validateElementText(String expectMessage) {
-        //String expectMessage = "You saved the address.";
         elementHelper.validateElementText(AccountLocators.saveAddressConfirmationElement,expectMessage);
     }
 
--- src/test/java/pages/RegisterPage.java
+++ src/test/java/pages/RegisterPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import helperMethods.ElementHelper;
+import loggerUtility.LoggerUtility;
 import org.openqa.selenium.WebDriver;
 import pageLocators.RegisterLocators;
 
@@ -22,27 +23,33 @@ public class RegisterPage {
 
     public void addFirstName(String firstNameValue) {
         elementHelper.fillLocator(RegisterLocators.firstNameElement, firstNameValue);
+        LoggerUtility.infoLog("Insert firstname");
     }
 
     public void addLastName(String lastNameValue) {
         elementHelper.fillLocator(RegisterLocators.lastNameElement, lastNameValue);
+        LoggerUtility.infoLog("Insert lastname");
     }
 
     public void addUserEmail() {
         String randomEmail = "user_" + UUID.randomUUID().toString().substring(0, 8) + "@example.com";
         elementHelper.fillLocator(RegisterLocators.emailElement, randomEmail);
+        LoggerUtility.infoLog("Insert email address");
     }
 
     public void addPassword(String passwordValue) {
         elementHelper.fillLocator(RegisterLocators.passwordElement, passwordValue);
+        LoggerUtility.infoLog("Insert password");
     }
 
     public void addConfirmationPassword(String confirmationPasswordValue) {
         elementHelper.fillLocator(RegisterLocators.passwordConfirmationElement, confirmationPasswordValue);
+        LoggerUtility.infoLog("Insert confirmation password");
     }
 
     public void createAccount() {
         elementHelper.clickLocator(RegisterLocators.submitButton);
+        LoggerUtility.infoLog("Press submit button");
     }
 
     public void thankYouMessage(String expectedMessage) {
